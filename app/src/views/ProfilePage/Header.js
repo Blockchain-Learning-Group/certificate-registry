ad381cab8801301cfc8292686fc350657ece72cc
Manik <manik.chaudhery@gmail.com>
2018-08-23 01:37:38 -0400

    Change in links colour

--- app/src/views/ProfilePage/Header.js
+++ app/src/views/ProfilePage/Header.js
@@ -23,8 +23,8 @@ class Header extends Component {
 
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About Us</a></li>
-	         <li><a className="smoothscroll" href="#resume"></a>Courses</li>
+            <li><a className="smoothscroll" href="#about">About</a></li>
+	         <li><a className="smoothscroll" href="#resume">Courses</a></li>
             <li><a className="smoothscroll" href="#portfolio">How it Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
