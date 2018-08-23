7f5ee602d9b5e5cfaf731a7a1e4c9b410f201694
Manik <manik.chaudhery@gmail.com>
2018-08-23 01:49:08 -0400

    Barebones updated

--- app/src/views/ProfilePage/ProfilePage.jsx
+++ app/src/views/ProfilePage/ProfilePage.jsx
@@ -165,7 +165,7 @@ import $ from 'jquery';
 import Header from './Header';
 import Footer from './Footer';
 import About from './About';
-import Resume from './Resume';
+import HowItWorks from './HowItWorks';
 import Contact from './Contact';
 import Testimonials from './Testimonials';
 import Portfolio from './Portfolio';
@@ -176,7 +176,7 @@ class ProfilePage extends Component {
     super(props);
     this.state = {
       foo: 'bar',
-      resumeData: {}
+      data: {}
     };
 
     ReactGA.initialize('UA-110570651-1');
@@ -184,13 +184,13 @@ class ProfilePage extends Component {
 
   }
 
-  getResumeData(){
+  getdata(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'/data.json',
       dataType:'json',
       cache: false,
       success: function(data){
-        this.setState({resumeData: data});
+        this.setState({data: data});
       }.bind(this),
       error: function(xhr, status, err){
         console.log(err);
@@ -200,19 +200,19 @@ class ProfilePage extends Component {
   }
 
   componentDidMount(){
-    this.getResumeData();
+    this.getdata();
   }
 
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+        <Header data={this.state.data.main}/>
+        <About data={this.state.data.main}/>
+        <HowItWorks data={this.state.data.resume}/>
+        <Portfolio data={this.state.data.portfolio}/>
+        <Testimonials data={this.state.data.testimonials}/>
+        <Contact data={this.state.data.main}/>
+        <Footer data={this.state.data.main}/>
       </div>
     );
   }
